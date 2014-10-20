c699af66a6b9fd99e2c83f74328ebcbe6b4385f2
Boczán Tamás <boczan.tamas@gmail.com>
2014-10-20 23:33:44 +0200

    structure

--- Source.cpp
+++ Source.cpp
@@ -66,76 +66,461 @@
 // 3D Vektor
 //--------------------------------------------------------
 struct Vector {
-   float x, y, z;
+    float x, y, z;
 
-   Vector( ) { 
-	x = y = z = 0;
-   }
-   Vector(float x0, float y0, float z0 = 0) { 
-	x = x0; y = y0; z = z0;
-   }
-   Vector operator*(float a) { 
-	return Vector(x * a, y * a, z * a); 
-   }
-   Vector operator+(const Vector& v) {
- 	return Vector(x + v.x, y + v.y, z + v.z); 
-   }
-   Vector operator-(const Vector& v) {
- 	return Vector(x - v.x, y - v.y, z - v.z); 
-   }
-   float operator*(const Vector& v) { 	// dot product
-	return (x * v.x + y * v.y + z * v.z); 
-   }
-   Vector operator%(const Vector& v) { 	// cross product
-	return Vector(y*v.z-z*v.y, z*v.x - x*v.z, x*v.y - y*v.x);
-   }
-   float Length() { return sqrt(x * x + y * y + z * z); }
+    Vector() {
+        x = y = z = 0;
+    }
+
+    Vector(float x0, float y0, float z0 = 0) {
+        x = x0;
+        y = y0;
+        z = z0;
+    }
+
+    Vector operator*(float a) {
+        return Vector(x * a, y * a, z * a);
+    }
+
+    Vector operator+(const Vector &v) {
+        return Vector(x + v.x, y + v.y, z + v.z);
+    }
+
+    Vector operator-(const Vector &v) {
+        return Vector(x - v.x, y - v.y, z - v.z);
+    }
+
+    float operator*(const Vector &v) {    // dot product
+        return (x * v.x + y * v.y + z * v.z);
+    }
+
+    Vector operator%(const Vector &v) {    // cross product
+        return Vector(y * v.z - z * v.y, z * v.x - x * v.z, x * v.y - y * v.x);
+    }
+
+    float Length() {
+        return sqrt(x * x + y * y + z * z);
+    }
 };
- 
+
 //--------------------------------------------------------
 // Spektrum illetve szin
 //--------------------------------------------------------
 struct Color {
-   float r, g, b;
+    float r, g, b;
 
-   Color( ) { 
-	r = g = b = 0;
-   }
-   Color(float r0, float g0, float b0) { 
-	r = r0; g = g0; b = b0;
-   }
-   Color operator*(float a) { 
-	return Color(r * a, g * a, b * a); 
-   }
-   Color operator*(const Color& c) { 
-	return Color(r * c.r, g * c.g, b * c.b); 
-   }
-   Color operator+(const Color& c) {
- 	return Color(r + c.r, g + c.g, b + c.b); 
-   }
+    Color() {
+        r = g = b = 0;
+    }
+
+    Color(float r0, float g0, float b0) {
+        r = r0;
+        g = g0;
+        b = b0;
+    }
+
+    Color operator*(float a) {
+        return Color(r * a, g * a, b * a);
+    }
+
+    Color operator*(const Color &c) {
+        return Color(r * c.r, g * c.g, b * c.b);
+    }
+
+    Color operator+(const Color &c) {
+        return Color(r + c.r, g + c.g, b + c.b);
+    }
 };
 
-const int screenWidth = 600;	// alkalmazás ablak felbontása
+const int screenWidth = 600;    // alkalmazás ablak felbontása
 const int screenHeight = 600;
 
+const size_t objectCount = 10;
+const size_t lightCount = 3;
 
-Color image[screenWidth*screenHeight];	// egy alkalmazás ablaknyi kép
+Color image[screenWidth * screenHeight];    // egy alkalmazás ablaknyi kép
 
+struct Ray {
+    Vector p0;
+    Vector v;
+};
+
+class Material {
+private:
+    Color F0, n, kd, ks;
+    float shine;
+    bool isReflective, isRefractive;
+public:
+    Material() {
+    }
+
+    Material(Color const &F0, Color const &n, Color const &kd, Color const &ks, float shine, bool isReflective, bool isRefractive)
+            : F0(F0), n(n), kd(kd), ks(ks), shine(shine), isReflective(isReflective), isRefractive(isRefractive) {
+    }
+
+    Color reflRadiance(Vector *l, Vector *n, Vector *v, Color *lIn) {
+        // TODO
+    }
+
+    Vector reflect(Vector *n, Vector *v) {
+        // TODO
+    }
+
+    Vector refract(Vector *n, Vector *v) {
+        // TODO
+    }
+
+    Color Fresnel(Vector *n, Vector *v) {
+        // TODO
+    }
+
+
+    Color const &getF0() const {
+        return F0;
+    }
+
+    void setF0(Color const &F0) {
+        Material::F0 = F0;
+    }
+
+    Color const &getN() const {
+        return n;
+    }
+
+    void setN(Color const &n) {
+        Material::n = n;
+    }
+
+    Color const &getKd() const {
+        return kd;
+    }
+
+    void setKd(Color const &kd) {
+        Material::kd = kd;
+    }
+
+    Color const &getKs() const {
+        return ks;
+    }
+
+    void setKs(Color const &ks) {
+        Material::ks = ks;
+    }
+
+    float getShine() const {
+        return shine;
+    }
+
+    void setShine(float shine) {
+        Material::shine = shine;
+    }
+
+    bool isIsReflective() const {
+        return isReflective;
+    }
+
+    void setIsReflective(bool isReflective) {
+        Material::isReflective = isReflective;
+    }
+
+    bool isIsRefractive() const {
+        return isRefractive;
+    }
+
+    void setIsRefractive(bool isRefractive) {
+        Material::isRefractive = isRefractive;
+    }
+};
+
+class Object {
+protected:
+    Material material;
+public:
+    Object() {
+    }
+
+    Object(Material const &material) : material(material) {
+    }
+
+    Material const &getMaterial() const {
+        return material;
+    }
+
+    void setMaterial(Material const &material) {
+        Object::material = material;
+    }
+
+    virtual Vector intersect(Ray *ray, Vector *normal) {
+    };
+
+};
+
+class Cylinder : Object {
+    Vector center, direction;
+    float radius, height;
+
+public:
+    Cylinder(Material const &material, Vector const &center, float radius, float height, Vector const &direction)
+            : Object(material), center(center), radius(radius), height(height), direction(direction) {
+    }
+
+    Vector intersect(Ray *ray, Vector *normal) {
+        // TODO
+    }
+
+    Vector const &getCenter() const {
+        return center;
+    }
+
+    void setCenter(Vector const &center) {
+        Cylinder::center = center;
+    }
+
+    float getRadius() const {
+        return radius;
+    }
+
+    void setRadius(float radius) {
+        Cylinder::radius = radius;
+    }
+
+    float getHeight() const {
+        return height;
+    }
+
+    void setHeight(float height) {
+        Cylinder::height = height;
+    }
+
+    Vector const &getDirection() const {
+        return direction;
+    }
+
+    void setDirection(Vector const &direction) {
+        Cylinder::direction = direction;
+    }
+
+};
+
+class Ellipsoid : Object {
+    Vector focus1;
+    Vector focus2;
+    float distance;
+
+
+public:
+    Ellipsoid(Material const &material, Vector const &focus1, Vector const &focus2, float distance)
+            : Object(material), focus1(focus1), focus2(focus2), distance(distance) {
+    }
+
+    Vector intersect(Ray *ray, Vector *normal) {
+        // TODO
+    }
+
+    Vector const &getFocus1() const {
+        return focus1;
+    }
+
+    void setFocus1(Vector const &focus1) {
+        Ellipsoid::focus1 = focus1;
+    }
+
+    Vector const &getFocus2() const {
+        return focus2;
+    }
+
+    void setFocus2(Vector const &focus2) {
+        Ellipsoid::focus2 = focus2;
+    }
+
+    float getDistance() const {
+        return distance;
+    }
+
+    void setDistance(float distance) {
+        Ellipsoid::distance = distance;
+    }
+};
+
+class Paraboloid : Object {
+    Vector plane, focus;
+    float height;
+
+public:
+    Paraboloid(Material const &material, Vector const &plane, Vector const &focus, float height)
+            : Object(material), plane(plane), focus(focus), height(height) {
+    }
+
+    Vector intersect(Ray *ray, Vector *normal) {
+        // TODO
+    }
+
+    Vector const &getPlane() const {
+        return plane;
+    }
+
+    void setPlane(Vector const &plane) {
+        Paraboloid::plane = plane;
+    }
+
+    Vector const &getFocus() const {
+        return focus;
+    }
+
+    void setFocus(Vector const &focus) {
+        Paraboloid::focus = focus;
+    }
+
+    float getHeight() const {
+        return height;
+    }
+
+    void setHeight(float height) {
+        Paraboloid::height = height;
+    }
+};
+
+class Camera {
+    Vector eye, lookat, up, right;
+    float width, height;
+
+public:
+    Camera(Vector const &eye, Vector const &lookat, Vector const &up, Vector const &right, float width, float height)
+            : eye(eye), lookat(lookat), up(up), right(right), width(width), height(height) {
+    }
+
+    Ray getRay(Vector *screeCoordinates) {
+        // TODO
+    }
+
+    Vector const &getEye() const {
+        return eye;
+    }
+
+    void setEye(Vector const &eye) {
+        Camera::eye = eye;
+    }
+
+    Vector const &getLookat() const {
+        return lookat;
+    }
+
+    void setLookat(Vector const &lookat) {
+        Camera::lookat = lookat;
+    }
+
+    Vector const &getUp() const {
+        return up;
+    }
+
+    void setUp(Vector const &up) {
+        Camera::up = up;
+    }
+
+    Vector const &getRight() const {
+        return right;
+    }
+
+    void setRight(Vector const &right) {
+        Camera::right = right;
+    }
+
+    float getWidth() const {
+        return width;
+    }
+
+    void setWidth(float width) {
+        Camera::width = width;
+    }
+
+    float getHeight() const {
+        return height;
+    }
+
+    void setHeight(float height) {
+        Camera::height = height;
+    }
+};
+
+class Light {
+    Vector p;
+
+// var?: lOut, type
+public:
+
+    Light() {
+    }
+
+    Light(Vector const &p) : p(p) {
+    }
+
+    Vector getDir() {
+        //TODO
+    }
+
+    Color getRad(Vector x) {
+        //TODO
+    }
+
+    Vector const &getP() const {
+        return p;
+    }
+
+    void setP(Vector const &p) {
+        Light::p = p;
+    }
+};
+
+class Scene {
+    Object objects[objectCount];
+    Light lights[lightCount];
+    size_t objectSize, lightSize;
+    Camera camera;
+
+    // method: render, trace, intersectAll
+
+public:
+    Scene(Camera const &camera) : camera(camera) {
+        objectSize = lightSize = 0;
+    }
+
+    void add(Object const &object) {
+        objects[objectSize++] = object;
+    }
+
+    void add(Light const &light) {
+        lights[lightSize++] = light;
+    }
+
+    void render() {
+        // TODO
+    }
+
+    Color trace(Ray *ray, int d) {
+        // TODO
+    }
+
+    // TODO intersectAll
+
+    void build() {
+        // TODO
+    }
+};
 
 // Inicializacio, a program futasanak kezdeten, az OpenGL kontextus letrehozasa utan hivodik meg (ld. main() fv.)
-void onInitialization( ) { 
-	glViewport(0, 0, screenWidth, screenHeight);
+void onInitialization() {
+    glViewport(0, 0, screenWidth, screenHeight);
 
     // Peldakent keszitunk egy kepet az operativ memoriaba
-    for(int Y = 0; Y < screenHeight; Y++)
-		for(int X = 0; X < screenWidth; X++)
-			image[Y*screenWidth + X] = Color((float)X/screenWidth, (float)Y/screenHeight, 0);
+    for (int Y = 0; Y < screenHeight; Y++)
+        for (int X = 0; X < screenWidth; X++)
+            image[Y * screenWidth + X] = Color((float) X / screenWidth, (float) Y / screenHeight, 0);
 
 }
 
 // Rajzolas, ha az alkalmazas ablak ervenytelenne valik, akkor ez a fuggveny hivodik meg
-void onDisplay( ) {
-    glClearColor(0.1f, 0.2f, 0.3f, 1.0f);		// torlesi szin beallitasa
+void onDisplay() {
+    glClearColor(0.1f, 0.2f, 0.3f, 1.0f);        // torlesi szin beallitasa
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); // kepernyo torles
 
     // ..
@@ -143,22 +528,22 @@ void onDisplay( ) {
     // Peldakent atmasoljuk a kepet a rasztertarba
     glDrawPixels(screenWidth, screenHeight, GL_RGB, GL_FLOAT, image);
     // Majd rajzolunk egy kek haromszoget
-	glColor3f(0, 0, 1);
-	glBegin(GL_TRIANGLES);
-		glVertex2f(-0.2f, -0.2f);
-		glVertex2f( 0.2f, -0.2f);
-		glVertex2f( 0.0f,  0.2f);
-	glEnd( );
+    glColor3f(0, 0, 1);
+    glBegin(GL_TRIANGLES);
+    glVertex2f(-0.2f, -0.2f);
+    glVertex2f(0.2f, -0.2f);
+    glVertex2f(0.0f, 0.2f);
+    glEnd();
 
     // ...
 
-    glutSwapBuffers();     				// Buffercsere: rajzolas vege
+    glutSwapBuffers();                    // Buffercsere: rajzolas vege
 
 }
 
 // Billentyuzet esemenyeket lekezelo fuggveny (lenyomas)
 void onKeyboard(unsigned char key, int x, int y) {
-    if (key == 'd') glutPostRedisplay( ); 		// d beture rajzold ujra a kepet
+    if (key == 'd') glutPostRedisplay();        // d beture rajzold ujra a kepet
 
 }
 
@@ -170,18 +555,17 @@ void onKeyboardUp(unsigned char key, int x, int y) {
 // Eger esemenyeket lekezelo fuggveny
 void onMouse(int button, int state, int x, int y) {
     if (button == GLUT_LEFT_BUTTON && state == GLUT_DOWN)   // A GLUT_LEFT_BUTTON / GLUT_RIGHT_BUTTON illetve GLUT_DOWN / GLUT_UP
-		glutPostRedisplay( ); 						 // Ilyenkor rajzold ujra a kepet
+        glutPostRedisplay();                         // Ilyenkor rajzold ujra a kepet
 }
 
 // Eger mozgast lekezelo fuggveny
-void onMouseMotion(int x, int y)
-{
+void onMouseMotion(int x, int y) {
 
 }
 
 // `Idle' esemenykezelo, jelzi, hogy az ido telik, az Idle esemenyek frekvenciajara csak a 0 a garantalt minimalis ertek
-void onIdle( ) {
-     long time = glutGet(GLUT_ELAPSED_TIME);		// program inditasa ota eltelt ido
+void onIdle() {
+    long time = glutGet(GLUT_ELAPSED_TIME);        // program inditasa ota eltelt ido
 
 }
 
